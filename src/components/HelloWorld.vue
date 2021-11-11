1eeb197534c958560fe682d13428c0cccd27f105
pipe <15283666096@163.com>
2021-11-11 22:32:19 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue";
+defineProps<{ msg: string }>();
+const count = ref(0);
+</script>
+
+<template>
+  <div class="flex justify-center flex-col items-center mt-5">
+    <h1 class="bg-red-500 cursor-pointer text-lg font-mono">{{ msg }}</h1>
+    <p class=" text-center mt-10">{{ count }}</p>
+    <button
+      class="rounded-full h-24 w-24 flex items-center justify-center bg-indigo-500 mt-10"
+      type="button"
+      @click="count++"
+    >加1</button>
+  </div>
+</template>
