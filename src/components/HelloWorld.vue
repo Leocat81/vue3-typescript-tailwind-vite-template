3ea887045416927b5d5fef7d3502579b0078d661
pipe <15283666096@163.com>
2021-12-26 23:25:59 +0800

    add:尝试事件流

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,42 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import request from '../serve/index';
+import request from "../serve/index";
 defineProps<{ msg: string }>();
 const count = ref(0);
-request.get('')
+request.get("");
+
+const source = new EventSource("api/sse");
+debugger;
+source.onopen = function (event) {
+  debugger;
+};
+
+source.onmessage = function (event) {
+  
+ console.log(event);
+ debugger
+};
 </script>
 
 <template>
   <div class="flex justify-center flex-col items-center mt-5">
     <h1 class="bg-red-500 cursor-pointer text-lg font-mono">{{ msg }}</h1>
-    <p class=" text-center mt-10">{{ count }}</p>
+    <p class="text-center mt-10">{{ count }}</p>
     <button
-      class="rounded-full h-24 w-24 flex items-center justify-center bg-indigo-500 mt-10"
+      class="
+        rounded-full
+        h-24
+        w-24
+        flex
+        items-center
+        justify-center
+        bg-indigo-500
+        mt-10
+      "
       type="button"
       @click="count++"
-    >加1</button>
+    >
+      加1
+    </button>
   </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import alias from "@rollup/plugin-alias";
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
+      port:3001,
     proxy: {
       '/api': {
-        target: 'http://pipepandafeng.gitee.io/blog_cover/',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
